39791a9bf53a39839aea76260bf990f3beb8052a
Rafal Dziedzic <rafal.dziedzic@op.pl>
2023-06-25 15:15:25 +0200

    dependencies configured (h2, web)

--- src/main/java/com/project/stundet/api/StudentApiApplication.java
+++ src/main/java/com/project/stundet/api/StudentApiApplication.java
@@ -2,12 +2,14 @@ package com.project.stundet.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @SpringBootApplication
+@EnableWebMvc
 public class StudentApiApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(StudentApiApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(StudentApiApplication.class, args);
+    }
 
 }
