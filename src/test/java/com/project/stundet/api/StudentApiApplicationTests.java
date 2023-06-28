1ca57cee3a3b5b49678b7fe65bacdb2f5c395c7f
Rafal Dziedzic <rafal.dziedzic@op.pl>
2023-06-28 19:46:40 +0200

    api test

--- src/test/java/com/project/stundet/api/StudentApiApplicationTests.java
+++ src/test/java/com/project/stundet/api/StudentApiApplicationTests.java
@@ -1,13 +1,29 @@
 package com.project.stundet.api;
 
+import com.project.stundet.api.dto.StatReportDto;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
 
-@SpringBootTest
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class StudentApiApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+
+    @Value(value = "${local.server.port}")
+    private int port;
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @Test
+    void contextLoads() {
+        assertThat(this.restTemplate.getForObject("http://localhost:" + port + "/students/stats/avg-age", StatReportDto.class))
+                .isNotInstanceOf(String.class);
+    }
+
 
 }
